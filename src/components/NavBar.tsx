6c077e9678ff4b04db2716c200312fd397bfeeb1
MSI/user <>
2023-12-10 19:49:01 +0200

    color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import  logo  from '../assets/react.svg';
+import ColorModeSwitch from './ColorModeSwitch';
 
 export default function Navbar() {
     return (
-        <HStack>
-            <Image src={logo} boxSize={'60px'} padding={'16px 0'} />
-            <Text> NavBar</Text>
+        <HStack justifyContent={'space-between'} padding={'16px'}>
+            <Image src={logo} boxSize={'40px'} />
+            <ColorModeSwitch />
         </HStack>
     )
 }
