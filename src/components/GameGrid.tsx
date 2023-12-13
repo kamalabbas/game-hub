e6af6cb81f000c35c8fe18191c677fac2cfb55e4
MSI/user <>
2023-12-13 14:23:43 +0200

    create a generic data  fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 export default function GameGrid() {
-    const { games, error, isLoading } = useGames();
+    const { data, error, isLoading } = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
@@ -13,7 +13,7 @@ export default function GameGrid() {
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='16px' spacing={10}>
                 {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>)}
 
-                {games.map(game => (
+                {data.map(game => (
                     <GameCardContainer>
                         <GameCard key={game.id} game={game} />
                     </GameCardContainer>
