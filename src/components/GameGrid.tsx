8c327e0f99d3f535e4c29002949da4e5ec83d10b
MSI/user <>
2023-12-13 13:56:23 +0200

    remove duplcate styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,8 @@
 import { SimpleGrid, Skeleton, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
-import GameCardSkeleton from './gameCardSkeleton';
+import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 export default function GameGrid() {
     const { games, error, isLoading } = useGames();
@@ -10,9 +11,12 @@ export default function GameGrid() {
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='16px' spacing={10}>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+                {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>)}
+
                 {games.map(game => (
-                    <GameCard key={game.id} game={game} />
+                    <GameCardContainer>
+                        <GameCard key={game.id} game={game} />
+                    </GameCardContainer>
                 ))}
             </SimpleGrid>
         </>
