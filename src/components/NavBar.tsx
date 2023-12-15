aa23a2a7c927ba1d772596681a377129d2c26bed
MSI/user <>
2023-12-15 23:45:43 +0200

    build serach input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react'
 import  logo  from '../assets/react.svg';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 export default function Navbar() {
     return (
-        <HStack justifyContent={'space-between'} padding={'16px'}>
+        <HStack padding={'16px'}>
             <Image src={logo} boxSize={'40px'} />
+            <SearchInput />
             <ColorModeSwitch />
         </HStack>
     )
